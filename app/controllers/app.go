a643b7a6eeeb135ffb2f201961dd330d6960f04e
Hunter Leath <h@hunterleath.com>
2014-02-11 17:45:20 -0500

    Added HoosBooks integration

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -6,12 +6,14 @@ import (
 	"github.com/huntaub/list/schedule"
 	"github.com/robfig/revel"
 	"net/http"
+	"regexp"
 	"strings"
 	"time"
 )
 
 var classList schedule.ClassList
 var lastUpdate time.Time
+var classRegex *regexp.Regexp
 
 func init() {
 	f := func(now time.Time) {
@@ -41,6 +43,7 @@ func init() {
 		}
 	}()
 	f(time.Now())
+	classRegex = regexp.MustCompile(`([A-z]{2,4})\s?(\d+)`)
 }
 
 type App struct {
@@ -72,12 +75,22 @@ func (c App) Class(dept string, num string) revel.Result {
 }
 
 func (c App) Search(class string) revel.Result {
-	_, ok := classList[class]
+	matches := classRegex.FindAllStringSubmatch(class, -1)
+	if len(matches) < 1 || len(matches[0]) < 3 {
+		return c.Redirect(routes.App.NotFound())
+	}
+	lookup := strings.ToUpper(matches[0][1]) + " " + matches[0][2]
+
+	_, ok := classList[lookup]
 
 	if !ok {
 		return c.Redirect(routes.App.NotFound())
 	} else {
-		comp := strings.Split(class, " ")
+		comp := strings.Split(lookup, " ")
 		return c.Redirect(routes.App.Class(comp[0], comp[1]))
 	}
+}
+
+func (c App) Build(classList string) revel.Result {
+	return c.Render()
 }
