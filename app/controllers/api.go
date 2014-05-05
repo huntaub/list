431e561ee4c32aa81b4db74ea91221dad86c2dca
Hunter Leath <h@hunterleath.com>
2014-05-05 10:24:48 -0400

    Fixed Stupid Things I Did During Merge

--- app/controllers/api.go
+++ app/controllers/api.go
@@ -11,3 +11,17 @@ type API struct {
 func (c API) Documentation() revel.Result {
 	return c.Render()
 }
+
+func (c API) Settings() revel.Result {
+	var u User
+	err := users.Find(map[string]string{"email": c.Session["user"]}).One(&u)
+	if err != nil {
+		panic(err)
+	}
+	c.RenderArgs["user"] = u
+	return c.Render()
+}
+
+func (c API) Class() revel.Result      { return c.Render() }
+func (c API) Department() revel.Result { return c.Render() }
+func (c API) Schedule() revel.Result   { return c.Render() }
